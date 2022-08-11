0cabacaf352c1f3fb2c8e8dcbbbcd4a867d8714f
KaioDev08 <Kaioaraujo6190@outlook.com>
2022-08-11 10:34:01 -0300

    Término de validação do formulário

--- js/main.js
+++ js/main.js
@@ -1,25 +1,47 @@
 const buttonSubmit = document.querySelector('#buttonSubmit');
 
+function buscaDataAtual(){
+    const data =  new Date();
+    dataAtual = data.getFullYear().toString();
+    dataAtual = dataAtual.substring(dataAtual.length - 2,dataAtual.length);
+
+    return dataAtual;
+
+}
+
+
 buttonSubmit.addEventListener('click', (event) => {
     event.preventDefault();
-
+    resetaClasses();
     validar();
-
-  
-
 
 })
 
-function validar(){
-     //const nome = document.querySelector('#titular');
-    // const numero = document.querySelector('#numero');
-    // const mes = document.querySelector('#mes');
-    // const ano = document.querySelector('#ano');
+function resetaClasses() {
+    //Removendo bordas 
 
-    // console.log('to aqui');
-    // console.log(nome.value);
-    // console.log(numero.value);
-    
+    titular.classList.remove('borderError')
+    numero.classList.remove('borderError')
+    mes.classList.remove('borderError')
+    ano.classList.remove('borderError')
+    cvv.classList.remove('borderError')
+    //Escondendo bloco de erro
+
+    const erroTitular = document.querySelector('#erro-titular');
+    const erroNumero = document.querySelector('#erro-numero');
+    const erroData = document.querySelector('#erro-data');
+    const erroCvv = document.querySelector('#erro-cvv');
+
+
+    erroTitular.classList.remove('visible')
+    erroNumero.classList.remove('visible')
+    erroData.classList.remove('visible')
+    erroCvv.classList.remove('visible')
+
+}
+
+function validar(){
+
     if(titular.value == ""){
         exibirErro(titular.id , "Titular inválido");
         
@@ -29,14 +51,38 @@ function validar(){
         exibirErro(numero.id , "Número de cartão inválido");
     };
 
+    if(mes.value == "" || mes.value > 12){
+        exibirErro(mes.id, "Data inválida");
+     }
+
+    if((ano.value.length == 0 || ano.value.length > 2) || ( ano.value < buscaDataAtual() ) ){
+        exibirErro(ano.id, "Data inválida");
+    }
+    if(cvv.value.length != 3){
+        exibirErro(cvv.id, "CVV inválido");
+    }
 }
 
 function exibirErro(id, msg){
-    const campoErro = document.querySelector(`input#${id}`)
-    const erro = document.querySelector(`#erro-${id}`);
-    erro.innerText = msg;
+    //selecionando o input a ser destado 
 
+    const campoErro = document.querySelector(`input#${id}`)
+    
+    //selecionando o bloco de erro a ser alterado
+    //caso seja um mês ou ano selecione o campo databa
+    //se não selecione o campo com o seu id 
+
+    let erro = ""
+    
+    if(id == "mes" || id == "ano"){
+        erro = document.querySelector(`#erro-data`);
+        
+    }else{
+        erro = document.querySelector(`#erro-${id}`);
+        
+    }
     erro.classList.add('visible');
-    campoErro.classList.add('outlineError');
+    campoErro.classList.add('borderError');
+    erro.innerText = msg;
     
 }
